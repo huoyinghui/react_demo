ef6633d67e00d3f2394a290023ed9d8d3968352c
huoyinghui <yhhuo@yunjinginc.com>
2019-01-15 07:55:38 +0800

    tac

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App';
-import MulClock from './components/Clock';
-import MarkdownEditor from './components/MarkdownEditor';
-import Welcom from './components/Welcom';
+// import App from './components/App';
+// import MulClock from './components/Clock';
+// import MarkdownEditor from './components/MarkdownEditor';
+// import Welcom from './components/Welcom';
+// import ShopList from './components/ShopList';
+import Game from './components/Tac';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-ReactDOM.render(<MulClock />, document.getElementById('example-clock'));
-ReactDOM.render(<MarkdownEditor />, document.getElementById('example-md'));
-ReactDOM.render(<Welcom name='chrome'/>, document.getElementById('example-welcom'));
-
+// ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<MulClock />, document.getElementById('example-clock'));
+// ReactDOM.render(<MarkdownEditor />, document.getElementById('example-md'));
+// ReactDOM.render(<Welcom name='chrome'/>, document.getElementById('example-welcom'));
+// ReactDOM.render(<ShopList name='chrome'/>, document.getElementById('example-shop'));
+ReactDOM.render(<Game />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
---
+++ web/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import * as serviceWorker from './serviceWorker';
+
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+// ReactDOM.render(<MulClock />, document.getElementById('example-clock'));
+// ReactDOM.render(<MarkdownEditor />, document.getElementById('example-md'));
+// ReactDOM.render(<Welcom name='chrome'/>, document.getElementById('example-welcom'));
+
+serviceWorker.unregister();
